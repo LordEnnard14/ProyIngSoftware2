db00b227fd0dd6253c46e6c32d0b535b33040c92
chuuxter <aldairramirez13@hotmail.com>
2024-10-05 00:30:32 -0500

    EndPoints relacionales, esperando poblacion de tablas

--- proyingsoftware2/BACK-END/Controladores/Productos.js
+++ proyingsoftware2/BACK-END/Controladores/Productos.js
@@ -1,6 +1,12 @@
 // Archivo: Controladores/Productos.js
 import express from "express";
 import Producto from "../models/Producto.js";
+import {
+    StockProducto,   
+    Marca,
+    Botica,    
+  } from "../models/Relaciones.js";
+import { Op } from "sequelize";
 
 
 const router = express.Router();
@@ -39,14 +45,99 @@ router.get('/detalle/:id', function(req, res) {
 
 router.get('/productoAll', async (req, res) => {
     try {
-        const result = await Producto.findAll(); // Consulta para obtener todos los productos
-        console.log(result); // Para depuración, si es necesario
-        res.json(result); // Respuesta directa en formato JSON
+        const result = await Producto.findAll({
+            include: [
+                {
+                    model: Marca,                    
+                    attributes: ['id', 'nombre'], 
+                },
+                {
+                    model: StockProducto, 
+                    include: {
+                        model: Botica,                         
+                        attributes: ['id', 'nombre', 'direccion'], 
+                    }
+                }
+            ]
+        });
+        
+        res.json(result); // Envío del resultado en formato JSON
     } catch (error) {
         console.error("Error al obtener los productos:", error);
         res.status(500).json({ error: "Error al obtener los productos" });
     }
 });
+
+router.get("/searchProduct", async (req, res) => {
+    const query = req.query.query.toLowerCase(); // Obtiene la consulta y la convierte a minúsculas
+    try {
+        const resultados = await Producto.findAll({
+            where: {
+                nombre: {
+                    [Op.iLike]: `%${query}%`, // Búsqueda insensible a mayúsculas/minúsculas por nombre del medicamento
+                }
+            },
+            include: [
+                {
+                    model: Marca,
+                    attributes: ['id', 'nombre'], // Incluye la marca
+                },
+                {
+                    model: StockProducto,
+                    include: {
+                        model: Botica,
+                        attributes: ['id', 'nombre', 'direccion'], // Incluye la botica
+                    }
+                }
+            ]
+        });
+
+        if (resultados.length > 0) {
+            res.json(resultados); // Devuelve los resultados encontrados
+        } else {
+            res.status(404).send("No se encontraron productos que coincidan con la búsqueda."); // Mensaje si no se encuentran productos
+        }
+    } catch (error) {
+        console.error("Error al buscar productos:", error);
+        res.status(500).send("Error al buscar productos."); // Manejo de errores
+    }
+});
+
+router.get('/detalle/:id', async (req, res) => {
+    const id = req.params.id; // Obtiene el ID del producto de los parámetros de la solicitud
+
+    try {
+        const producto = await Producto.findByPk(id, {
+            include: [
+                {
+                    model: Marca,
+                    attributes: ['id', 'nombre'], // Incluye la marca del producto
+                },
+                {
+                    model: StockProducto,
+                    include: {
+                        model: Botica,
+                        attributes: ['id', 'nombre', 'direccion'], // Incluye la botica
+                    }
+                }
+            ]
+        });
+
+        if (producto) {
+            res.json(producto); // Devuelve el producto encontrado
+        } else {
+            res.status(404).json({ error: "Producto no encontrado" }); // Mensaje si no se encuentra el producto
+        }
+    } catch (error) {
+        console.error("Error al obtener el producto:", error);
+        res.status(500).send("Error al obtener el producto."); // Manejo de errores
+    }
+});
+
+
+
+
+
 
 
 
