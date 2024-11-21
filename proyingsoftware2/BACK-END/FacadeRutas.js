563cb1cd681aa0a51c065e163386ae644ff7bc4f
AlekseyCP <alekseycp@gmail.com>
2024-11-21 17:11:30 -0500

    Ya se creo el modelo del AdminMaestro

--- proyingsoftware2/BACK-END/FacadeRutas.js
+++ proyingsoftware2/BACK-END/FacadeRutas.js
@@ -6,7 +6,8 @@ import productoDetalleRoutes from "./Controladores/ProductosDetalle.js";
 import carritoRoutes from "./Controladores/Carrito.js";
 import ordenesRoutes from "./Controladores/Carrito.js";
 import boticaRoutes from "./Controladores/Botica.js";
-import marcasRoutes from "./Controladores/Marcas.js"
+import marcasRoutes from "./Controladores/Marcas.js";
+import adminGeneralRoutes from "./Controladores/AdminGeneral.js";
 
 export default function(app){
     app.use("/api/productos", productoRoutes);
@@ -18,5 +19,5 @@ export default function(app){
     app.use("/api/carrito", carritoRoutes);
     app.use("/api/ordenes", ordenesRoutes);
     app.use("/api/marcas",marcasRoutes);   
-    
+    app.use("/api/adminMaestro", adminGeneralRoutes);
 }
